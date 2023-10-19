4d8d63e4ead1884ae8491f5b56f215e77bf6ca74
Sebastien <byasebastien@hotmail.com>
2023-10-19 13:23:55 +0200

    second commit

---
+++ DemoMultiModuleProject_PL/src/main/java/be/bstorm/demomultimoduleproject/configs/SpringJpaConfig.java
@@ -0,0 +1,12 @@
+package be.bstorm.demomultimoduleproject.configs;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+//@EnableJpaRepositories(basePackages = "")
+@EntityScan(basePackages = "entities")
+public class SpringJpaConfig {
+    // Votre configuration Spring JPA ici
+}
